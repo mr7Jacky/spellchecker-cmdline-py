63b8d28c8e78d42e3b75e327f340984d13b1923b
jacky <jacky@jacky-ubuntu-20.04>
2020-11-04 22:50:33 -0500

    initialize the run.py

---
+++ src/run.py
@@ -0,0 +1,27 @@
+import sys
+import Checker
+
+def print_instruction():
+    print("Usage: run.py [type] [input]")
+    print("Parameter [type] can be either 1 for string or 2 for text file.")
+    print("If type is 1, enter the string in [input].")
+    print("If type is 2, enter the path to [input] text file.")
+
+def main():
+    if len(sys.argv) != 3:
+		print_instruction()
+        sys.exit()
+    in_type = sys.argv[1]
+    usr_in = sys.argv[2]
+    if in_type == 1:
+        cor_str = Checker.str_checker(usr_in)
+        print("Correction: %s" % (cor_str))
+    elif in_type == 2:
+    	Checker.file_checker(usr_in)
+    	print("Saving corrected file to %s_cort.txt" % (usr_in[:-4]))
+    else:
+        print("Error: Invalid Type.")
+        print_instruction()
+
+if __name__ == "__main__":
+    main()
