ff8aa86ca59ee17d309d7ef17c1199e9675c6812
jacky <jacky@jacky-ubuntu-20.04>
2020-11-09 23:32:13 -0500

    create a template for Checker function & add existing function to the class

--- src/run.py
+++ src/run.py
@@ -1,5 +1,6 @@
 import sys
-import Checker
+from . import Checker
+
 
 def print_instruction():
     print("Usage: run.py [type] [input]")
@@ -7,9 +8,10 @@ def print_instruction():
     print("If type is 1, enter the string in [input].")
     print("If type is 2, enter the path to [input] text file.")
 
+
 def main():
     if len(sys.argv) != 3:
-	print_instruction()
+        print_instruction()
         sys.exit()
     in_type = sys.argv[1]
     usr_in = sys.argv[2]
@@ -17,11 +19,12 @@ def main():
         cor_str = Checker.str_checker(usr_in)
         print("Correction: %s" % (cor_str))
     elif in_type == 2:
-    	Checker.file_checker(usr_in)
-    	print("Saving corrected file to %s_cort.txt" % (usr_in[:-4]))
+        Checker.file_checker(usr_in)
+        print("Saving corrected file to %s_cort.txt" % (usr_in[:-4]))
     else:
         print("Error: Invalid Type.")
         print_instruction()
+
 
 if __name__ == "__main__":
     main()
