e050fc206b2e5c546748a0a70c2f2c25e172a4aa
jacky <jacky@jacky-ubuntu-20.04>
2020-11-10 07:57:13 -0500

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # CSCI Final Project - Spell Checker
+
+#### How to use
+
+Run the `run.py` file with the following command
+
+```bash
+python run.py [mode] [input]
+```
+
+The first input is `mode` indicating whether to check file or string
+
+1. Mode 1: Check the string
+2. Mode 2: check the a `.txt` file
+
+The second input is the string or the `.txt` file you want to check, it is either a sentence or the path to the text file.
--- run.py
+++ run.py
@@ -1,25 +1,26 @@
 import sys
-from . import Checker
+from .src import Checker
 
 
 def print_instruction():
-    print("Usage: run.py [type] [input]")
-    print("Parameter [type] can be either 1 for string or 2 for text file.")
-    print("If type is 1, enter the string in [input].")
-    print("If type is 2, enter the path to [input] text file.")
+    print("Usage: run.py [mode] [input]")
+    print("Parameter [mode] can be either 1 for string or 2 for text file.")
+    print("If mode is 1, enter the string in [input].")
+    print("If mode is 2, enter the path to [input] text file.")
 
 
 def main():
     if len(sys.argv) != 3:
         print_instruction()
         sys.exit()
+    chk = Checker(wordlist)
     in_type = sys.argv[1]
     usr_in = sys.argv[2]
     if in_type == 1:
-        cor_str = Checker.str_checker(usr_in)
+        cor_str = chk.str_checker(usr_in)
         print("Correction: %s" % (cor_str))
     elif in_type == 2:
-        Checker.file_checker(usr_in)
+        chk.file_checker(usr_in)
         print("Saving corrected file to %s_cort.txt" % (usr_in[:-4]))
     else:
         print("Error: Invalid Type.")
