a1faea523a4b8147bd32978f20a904628c309302
jacky <jacky@jacky-ubuntu-20.04>
2020-11-13 10:29:25 -0500

    hashtable 2d finished

--- src/HashTable2D.py
+++ src/HashTable2D.py
@@ -9,19 +9,22 @@ class HashTable2D:
         self.hash_tables = [HashTable(20) for _ in range(26)]
 
     def __str__(self):
+        """
+        TODO - NOT WORKING
+        """
         s = ''
         for i in range(len(self.hash_tables)):
             s += ("Table %s\n" % (chr(i + ord('a'))))
-            s += i.__str__()
+
         return s
 
     # target for every value.
     @staticmethod
     def __hash(key):
         """
-        @type key: Word
+        @type key: str
         """
-        return ord(key.word[0]) - ord('a')
+        return ord(key[0]) - ord('a')
 
     # Insert Function to add
     # values to the hash table
@@ -32,7 +35,7 @@ class HashTable2D:
         @type ascii_sum: int
         """
         newKey = Word(key, freq)
-        idx = self.__hash(newKey)
+        idx = self.__hash(key)
         self.hash_tables[idx].add(newKey)
 
     def search(self, target):
@@ -41,14 +44,17 @@ class HashTable2D:
 
 
 def fill_hash_table(h):
-    with open('dict/allWords.csv') as csvfile:
+    """
+    @type h: HashTable2D
+    """
+    with open('../dict/allWords.csv') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
+        count = 0
         for row in readCSV:
-            ascii_sum = 0
             freq = int(''.join(row[2].split(',')))
-            for i in range(0, int(len(row[0]) / 2) + 1):
-                ascii_sum += ord(row[0][i])
-            # insert(ascii_sum, (row[0].lower(), freq, ascii_sum))
+            count += 1
+            print("Insert %dth %s" % (count,row[0]))
+            h.insert(row[0].lower(), freq)
 
 
 if __name__ == "__main__":
@@ -56,4 +62,11 @@ if __name__ == "__main__":
     # number_of_words = size_of_table()
     # print(number_of_words)
     h = HashTable2D()  # initialize the hash table
+    h.insert("haha",1)
+    h.insert("ahah",2)
+    h.insert("hehe",3)
+    h.insert("hahz",4)
+    h.insert('b',5)
     print(h)
+    print(h.search('hahz'))
+    print(h.search('a'))
